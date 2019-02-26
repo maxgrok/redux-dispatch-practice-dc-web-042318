0e2d51cf7445a2f97c8b6b5a8bf50b9277db001c
Max Goodman <max.goodman@flatironschool.com>
2019-02-26 14:09:18 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,37 @@
 export let state;
+let counter = 0;
 
-
-export function managePets(){
+export function managePets(state ={pets: []}, action){
+    switch(action.type){
+        case "ADD_PET":
+            state = {pets: [...state.pets, action.pet]}
+            return state
+        case "REMOVE_PET":
+            let newState = [{pets: [...state.pets]}]
+            let remove = newState[0].pets.filter((pet)=>{
+            if (pet.id !== action.id){
+                return pet
+            }
+        })
+        return {pets: remove}
+    default:
+    return state;
+    }
 }
 
-export function dispatch(){
+export function dispatch(action){
+   state = managePets(state, action)
+    render()
 }
 
 export function render(){
-
-}
+    let div = document.querySelector("#container")
+    if(counter < state.pets.length){
+        let list = document.createElement("ul") 
+        let listItem = document.createElement("li")
+        listItem.innerHTML = `${state.pets[counter].name}`
+        div.appendChild(list)
+         list.appendChild(listItem)
+         counter+=1;
+    }  
+   } 
